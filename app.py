3077ee03ed9257b24414534f3f59e000f3ddef30
aaaaa230 <sanjingkuanren@gmail.com>
2025-08-20 10:52:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ render()
 genai.configure(api_key=api)
 
 st.write("###### 1.入力欄に変換したい文を入力  \n###### 2.変換ボタンをクリック  \n###### 3.中二病チックな文が生成されます")
-st.write("###### 2.変換ボタンをクリック")
-st.write("###### 3.中二病チックな文が生成されます")
+
 
 
 text_input = st.text_input('###### 文の入力', placeholder="例：このラーメンは実は食品サンプルです")
