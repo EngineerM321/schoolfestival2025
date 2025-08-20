0c12799699c0984846c9ddf0f86b11285574ce79
aaaaa230 <sanjingkuanren@gmail.com>
2025-08-20 10:50:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ def gemini(word):
 render()
 genai.configure(api_key=api)
 
-st.write("1.入力欄に変換したい文を入力")
-st.write("2.変換ボタンをクリック")
-st.write("3.中二病チックな文が生成されます")
+st.write("###### 1.入力欄に変換したい文を入力")
+st.write("###### 2.変換ボタンをクリック")
+st.write("###### 3.中二病チックな文が生成されます")
 
 
-text_input = st.text_input('文の入力', placeholder="例：このラーメンは実は食品サンプルです")
+text_input = st.text_input('###### 文の入力', placeholder="例：このラーメンは実は食品サンプルです")
 
 if text_input=="":
   text_input="例：このラーメンは実は食品サンプルです"
