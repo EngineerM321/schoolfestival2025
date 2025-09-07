db3fc055d738023969dd30abd9c972c83bf441dc
EngineerM <sanjingkuanren@gmail.com>
2025-09-07 19:41:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import google.generativeai as genai
 import pyttsx3
 
+maxword = 20
 api = st.secrets['secrets']['API_KEY']
 genai.configure(api_key=api)
 model = genai.GenerativeModel(model_name='gemini-2.0-flash-lite')
@@ -12,7 +13,7 @@ back = st.secrets['secrets']['Background_Image']
 #back='https://drive.google.com/uc?export=view&id=18jokNW8ktwfdnb5-3-n7XA7KwtftbVIr'
 st.title('中二病ジェネレーター')
 
-def render()->st:
+def render():
     bg_img = '<style>.stApp {background-image: url(' + back +');background-repeat: no-repeat;}</style>'
     return st.markdown(bg_img, unsafe_allow_html=True)
 
@@ -30,7 +31,6 @@ def read_text(text):
 
 
 render()
-genai.configure(api_key=api)
 
 st.write("###### 1.入力欄に変換したい文を入力  \n###### 2.変換ボタンをクリック  \n###### 3.中二病チックな文が生成されます")
 
@@ -38,12 +38,14 @@ st.write("###### 1.入力欄に変換したい文を入力  \n###### 2.変換ボ
 
 text_input = st.text_input('###### 文の入力', placeholder="例：このラーメンは実は食品サンプルです")
 
-if text_input=="":
-  text_input="このラーメンは実は食品サンプルです"
 
 if st.button('変換'):
-    with st.spinner('変換中...'):
-        time.sleep(3)
-        result = gemini(text_input)
-        st.write(result)
-        read_text(result)
+    if len(text_input) > maxword:
+        overword = len(text_input) - maxwword
+        st.warning(str(overword) + "文字オーバーしています")
+    else:
+        with st.spinner('変換中...'):
+            time.sleep(3)
+            result = gemini(text_input)
+            st.write(result)
+
