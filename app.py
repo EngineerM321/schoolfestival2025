09e7a137f277e9b9bd00f8dc3d4195ef59b0c713
EngineerM <sanjingkuanren@gmail.com>
2025-09-08 16:58:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,12 @@ def render():
 
 #フォントを指定する関数
 def font():
-    return st.markdown('<link href="https://fonts.googleapis.com/css2?family=WDXL+Lubrifont+JP+N&display=swap" rel="stylesheet">'
-    '<style>.stApp{'
-    'html, body, [class*="css"] {'
+    return st.markdown('''<link href="https://fonts.googleapis.com/css2?family=WDXL+Lubrifont+JP+N&display=swap" rel="stylesheet">'
+    <style>.stApp{
+    html, body, [class*="css"] {
     font-family: "WDXL Lubrifont JP N", sans-serif;
-    '}'
-    '}</style>', unsafe_allow_html=True)
+    }
+    }</style>''', unsafe_allow_html=True)
 
 # 入力された文を中二病風の単語に変換する関数
 def gemini(word):
