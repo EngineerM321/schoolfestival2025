06aec98304cbbbef42e72ecf8787fc669ff32941
EngineerM <sanjingkuanren@gmail.com>
2025-09-08 20:56:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ st.title('中二病ジェネレーター')
 
 # 背景画像を適用する関数
 def render():
-    bg_img = '''
+    bg_img = """
     <style>.stApp {
-    background-image: url(' + back +');
+    background-image: url({back});
     background-repeat: no-repeat;
-    }</style>'''
+    }</style>"""
     return st.markdown(bg_img, unsafe_allow_html=True)
 
 #フォントを指定する関数
