758dd634694cd02727d165596751eb065703f358
EngineerM <sanjingkuanren@gmail.com>
2025-09-09 22:03:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,19 +25,25 @@ def render():
 
 #フォントを指定する関数
 def font():
-    font_css = """
-    @font-face {
-        font-family: 'WDXL Lubrifont JP N';
-        src: url('./static/fonts/WDXLLubrifontJPN-Regular.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
-    body, .css-1v3fvcr {
-        font-family: 'WDXL Lubrifont JP N', sans-serif;
-    }
-    """
+    font_path = "./static/fonts/WDXLLubrifontJPN-Regular.woff2"
+    with open(font_path, "rb") as f:
+        font_data = f.read()
+        encoded = base64.b64encode(font_data).decode()
+
+    # CSSでフォントを読み込む
+    st.markdown(f"""
+        <style>
+        @font-face {{
+            font-family: 'WDXL Lubrifont JP N';
+            src: url(data:font/woff2;base64,{encoded}) format('truetype');
+        }}
     
-    return st.markdown(f'<style>.stApp{font_css}</style>', unsafe_allow_html=True)
+        html, body, [class*="css"]  {{
+            font-family: 'WDXL Lubrifont JP N', sans-serif;
+        }}
+        </style>
+    """, unsafe_allow_html=True)
+
 
 # 背景をレンダリング
 
