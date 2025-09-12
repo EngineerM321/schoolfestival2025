cd43de11e8a0f978dada4febc51542a26650d0ca
EngineerM <sanjingkuanren@gmail.com>
2025-09-12 14:59:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ if st.button('変換'):
         with st.spinner('変換中...'):
             time.sleep(3)  # スピナー表示のための待機時間
             result = gemini(text_input)
-            st.success("生成された中二病ワード:" + result)
+            st.success("結果:" + result)
